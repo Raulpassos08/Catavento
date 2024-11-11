fa18657c9a594ebec96e5dc587fe317f8bc62eca
Raulpassos08 <raulfera2@gmail.com>
2024-11-11 12:55:53 -0300

    sobre completo

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { NgOptimizedImage } from '@angular/common';
+import { SobreComponent } from "../sobre/sobre.component";
+import { ServicosComponent } from '../servicos/servicos.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage],
+  imports: [HeaderComponent, NgOptimizedImage, SobreComponent, ServicosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/componentes/sobre/sobre.component.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './sobre.component.html',
+  styleUrl: './sobre.component.scss'
+})
+export class SobreComponent {
+
+}
