25b3edb19f7b04fcbe3216ecb8d5a667cad92b60
Raulpassos08 <raulfera2@gmail.com>
2025-01-09 10:04:14 -0300

    add img from component contato

--- src/app/componentes/contato/contato.component.ts
+++ src/app/componentes/contato/contato.component.ts
@@ -1,10 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.scss'
 })
