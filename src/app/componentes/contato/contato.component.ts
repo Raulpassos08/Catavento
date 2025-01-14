a0b7316741e45aead530c36dfc5343355a805f4d
Raulpassos08 <raulfera2@gmail.com>
2025-01-14 10:59:56 -0300

    add the map and link icons

--- src/app/componentes/contato/contato.component.ts
+++ src/app/componentes/contato/contato.component.ts
@@ -1,14 +1,22 @@
-import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, GoogleMapsModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.scss'
 })
 export class ContatoComponent {
+  // center: google.maps.LatLngLiteral = { lat: -23.55052, lng: -46.633308 }; 
+  // zoom = 13;
+  options: google.maps.MapOptions = {
+    mapId: "DEMO_MAP_ID",
+    center: { lat: -3.8721051, lng: -38.613637},
+    zoom: 16,
+  };
+
+  markerPosition: google.maps.LatLngLiteral = { lat: -3.8721051, lng: -38.613637}
 }
